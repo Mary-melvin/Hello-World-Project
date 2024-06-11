ef4d3db5df16ea17888ccf6f463bdeb6b341e99c
Mary-melvin <162853197+Mary-melvin@users.noreply.github.com>
2024-06-11 23:06:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+import chalk from "chalk";
+console.log(chalk.green.bold("\n \tHello World \n"));
---
+++ main.ts
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+
+console.log(chalk.green.bold("\n \tHello World \n"));
